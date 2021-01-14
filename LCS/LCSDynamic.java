fb26565360edfc96e8b43d7381eec7bf7d5cf1f5
roei-birger <roei1237@gmail.com>
2021-01-14 10:03:30 +0200

    LCS dynamic part

---
+++ LCS/LCSDynamic.java
@@ -0,0 +1,31 @@
+package LCS;
+
+public class LCSDynamic {
+
+    public static void main(String[] args) {
+        String x = "abcbdab";
+        String y = "bdcaba";
+        int ans = LCS(x, y);
+
+        System.out.println(ans);
+    }
+
+    public static int LCS(String x, String y) {
+        int [][] helpTable = new int[y.length()+1][x.length()+1];
+        for (int i = 0; i<helpTable.length;i++){
+            for (int j = 0; j<helpTable[0].length;j++){
+                if (i==0 || j==0)
+                    helpTable[i][j] = 0;
+                else if (y.charAt(i-1) == x.charAt(j-1))
+                    helpTable[i][j] = helpTable[i-1][j-1]+1;
+                else
+                    helpTable[i][j] = Math.max(helpTable[i-1][j],helpTable[i][j-1]);
+            }
+        }
+            return helpTable[y.length()][x.length()];
+
+
+    }
+
+
+}
