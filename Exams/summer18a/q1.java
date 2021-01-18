2248b615a9b6ac8e21eda098beaa4f2a3f783722
Reut-Maslansky <maslanskyr@gmail.com>
2021-01-18 10:06:47 +0200

    q1

--- Exams/summer18a/q1.java
+++ Exams/summer18a/q1.java
@@ -8,11 +8,44 @@ public class q1 {
         int max = LIS(arr);
         int[] maxArr = LISArray(arr);
         System.out.println(max);
+        System.out.println(Arrays.toString(maxArr));
+        int[] array = {1, 4, 3, 5, 7, 10, 8, 13, 16};
     }
 
     private static int[] LISArray(int[] arr) {
+        int n = arr.length;
+        int[] sorted = new int[n];
+        System.arraycopy(arr, 0, sorted, 0, n);
+        Arrays.sort(sorted);
 
-        return null;
+        int[][] help = new int[n + 1][n + 1];
+
+        for (int i = 0; i < n + 1; i++) {
+            for (int j = 0; j < n + 1; j++) {
+                if (i == 0 || j == 0)
+                    help[i][j] = 0;
+                else {
+                    if (arr[i - 1] == sorted[j - 1])
+                        help[i][j] = help[i - 1][j - 1] + 1;
+                    else help[i][j] = Math.max(help[i][j - 1], help[i - 1][j]);
+                }
+            }
+        }
+
+        int[] ans = new int[help[n][n]];
+        int i = n, j = n, index = help[n][n] - 1;
+
+        while (help[i][j] > 0) {
+            if (arr[i - 1] == sorted[j - 1]) {
+                ans[index--] = arr[i - 1];
+                i--;
+                j--;
+            } else if (help[i - 1][j] > help[i][j - 1])
+                i--;
+            else j--;
+        }
+
+        return ans;
     }
 
     private static int LIS(int[] arr) {
@@ -33,36 +66,4 @@ public class q1 {
 
         return index + 1;
     }
-
-    private static int AbsLIS(int[] arr, int k) {
-        int[] help = new int[arr.length];
-        int index = 0;
-        help[0] = arr[0];
-        for (int i = 1; i < arr.length; i++) {
-            if (Math.abs(arr[i] - help[index]) == k)
-                help[++index] = arr[i];
-            else {
-                int ind2 = index;
-                int[] help2 = new int[arr.length];
-                boolean flag=false;
-                while (ind2 >= 0) {
-                    if (Math.abs(arr[i] - help[ind2]) == k) {
-                        flag=true;
-                        for (int j = 0; j < ind2; j++) {
-                            help2[j] = help[i];
-                        }
-                        help2[ind2] = arr[i];
-                    }
-                    else ind2--;
-                }
-
-            }
-        }
-
-        return index + 1;
-    }
-
-    private static void binarySearchBetween() {
-    }
-
 }
